296e838d3cb7a575aee14930b94ed92ccf7038ea
iankleinfeld <ian@evilgeniuscreative.com>
2025-09-01 18:05:06 -0400

    Changed audio volume

--- src/components/homepage/homepageRolloverImage.jsx
+++ src/components/homepage/homepageRolloverImage.jsx
@@ -5,18 +5,15 @@ import "./styles/homepageRolloverImage.css";
 const HomepageRolloverImage = () => {
 	const audioRef = useRef(null);
 
-	const [currentImage, setCurrentImage] = useState(INFO.main.hero1);
-
-	const handleImageChange = () => {
-		setTimeout(() => {
-			setCurrentImage(INFO.main.hero2);
-		}, 500);
-	};
+	const [isHovered, setIsHovered] = useState(false);
 
 	const handleMouseEnter = () => {
-		handleImageChange();
+		setTimeout(() => {
+			setIsHovered(true);
+		}, 500);
 		if (audioRef.current) {
 			audioRef.current.currentTime = 0; // Reset to beginning
+			audioRef.current.volume = 0.2;
 			audioRef.current
 				.play()
 				.catch((e) => console.log("Audio play failed:", e));
@@ -24,7 +21,7 @@ const HomepageRolloverImage = () => {
 	};
 
 	const handleMouseLeave = () => {
-		setCurrentImage(INFO.main.hero1); // Reset to original image
+		setIsHovered(false);
 		if (audioRef.current) {
 			audioRef.current.pause();
 			audioRef.current.currentTime = 0; // Reset to beginning
@@ -43,15 +40,19 @@ const HomepageRolloverImage = () => {
 						<img src="/images/batman.png" alt="batman" />
 					</div>
 					<img
-						src={`/images/${currentImage}`}
+						src={`/images/${INFO.main.hero1}`}
 						alt="hero main"
-						className="homepage-rollover"
+						className={`homepage-rollover-image ${
+							!isHovered ? "visible" : "hidden"
+						}`}
 						draggable="false"
 					/>
 					<img
-						src={`/images/${currentImage}`}
+						src={`/images/${INFO.main.hero2}`}
 						alt="hero rollover"
-						className="homepage-rollover"
+						className={`homepage-rollover-image ${
+							isHovered ? "visible" : "hidden"
+						}`}
 						draggable="false"
 					/>
 				</div>
@@ -60,10 +61,7 @@ const HomepageRolloverImage = () => {
 				<a href={INFO.main.herolink}>Click the image to get started</a>
 			</p>
 			<audio ref={audioRef} preload="auto">
-				<source
-					src="/server_assets/images/batman.mp3"
-					type="audio/mpeg"
-				/>
+				<source src="/images/batman.mp3" type="audio/mpeg" />
 				Your browser does not support the audio element.
 			</audio>
 		</div>
